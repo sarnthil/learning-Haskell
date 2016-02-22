6f619d21c7fc302ab0b7fd1d5df8b1810c9f7fa9
Laura Bostan <sarnthil@gmail.com>
2016-02-22 01:19:25 +0100

    playing with split module

---
+++ split.hs
@@ -0,0 +1,11 @@
+import Data.List.Split (splitOn)
+main = do
+  input <- readFile "terry.txt"
+  let ls = lines input
+  print $ ls
+  let ws = words $ ls !! 2
+  print ws
+  let cs = splitOn "," $ ls !! 3
+  print cs
+  let ds = splitOn "an" $ ls !! 4
+  print ds
